faa17460dbc28ed98c6892d7e9451b22b22831ce
Jon Skeen <jonskeen@gmail.com>
2019-11-23 15:26:39 -0800

    Rough draft Product update

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,16 +1,41 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Product = ({ price, inventory, title }) => (
-  <div>
-    {title} - &#36;{price}{inventory ? ` x ${inventory}` : null}
-  </div>
-)
+import styles from "./styles.module.css";
+
+
+const Product = ({ price, inventory, title }) => {
+  const imageSrc = require(`globals/assets/product-images/${title.toLowerCase()}.png`);
+  const hasInventory = inventory > 0;
+
+  return (
+      <div className={styles.product}>
+        <div className={styles.image} style={{ backgroundImage: `url(${imageSrc})` }}>
+          <div className={styles.aspectWrapper} />
+        </div>
+
+        <div className={styles.description}>
+          <div className={styles.titlePriceWrapper}>
+            <h2 className={styles.title}>{title}</h2>
+            <div className={styles.price}>{price}</div>
+          </div>
+
+          <div className={`${styles.inventory} ${!hasInventory ? styles.disabled : ""}`}>
+            {hasInventory
+                ? `${inventory} Remaining`
+                : "Out of Stock"
+            }
+          </div>
+        </div>
+
+      </div>
+  );
+};
 
 Product.propTypes = {
   price: PropTypes.number,
   inventory: PropTypes.number,
   title: PropTypes.string
-}
+};
 
 export default Product
