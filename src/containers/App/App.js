08ccdcd6a82d3fe2cf422b227ce28c0760fb675c
Jon Skeen <jonskeen@gmail.com>
2019-11-24 10:33:49 -0800

    Styling headline

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,18 +1,24 @@
-import React from 'react'
-import ProductsContainer from 'containers/Products/Products'
-import CartContainer from 'containers/CartContainer'
+import React from 'react';
+import ProductsContainer from 'containers/Products/Products';
+import CartContainer from 'containers/CartContainer';
+import CartIcon from 'globals/assets/icons/cart.svg'
 
-import styles from "./styles.module.css";
-
+import styles from './styles.module.css';
 
 const App = () => (
-  <div className={styles.app}>
-    <h1>Acme Store</h1>
-    <hr/>
-    <ProductsContainer />
-    <hr/>
-    <CartContainer />
-  </div>
+    <div className={styles.app}>
+      <div className={styles.headline}>
+        <h1>Acme Store</h1>
+        <button className={`${styles.cartWrapper} ${styles.unbutton}`}>
+          <CartIcon />
+          Your cart is empty
+        </button>
+      </div>
+      <hr className={styles.hRule}/>
+      <ProductsContainer/>
+      <hr/>
+      <CartContainer/>
+    </div>
 );
 
-export default App
+export default App;
